f3645a0055d5c4672da0b18ef06e27ee19ef7b37
Mary Bellavia <sauerwein.mary@gmail.com>
2020-12-11 21:49:57 -0600

    starting beginning of homework

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "a9e07d6efc15701fac8b274f76ceb8f6"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBOhyRLS4-ylbojgukDmjfrPaMz-TLwgyM"
